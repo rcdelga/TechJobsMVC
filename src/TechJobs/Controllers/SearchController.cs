ffec201e9402d333c6442ec58f593f9c51f64e70
Rob Delgado <triptix@hotmail.com>
2018-11-22 14:31:57 -0600

    Completed without Bonus Missions

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -16,5 +16,22 @@ namespace TechJobs.Controllers
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
+
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Dictionary<string, string>> jobList = new List<Dictionary<string, string>>();
+            if (searchType.Equals("all"))
+            {
+                jobList = JobData.FindByValue(searchTerm);
+            }
+            else
+            {
+                jobList = JobData.FindByColumnAndValue(searchType, searchTerm);
+            }
+            ViewBag.jobList = jobList;
+            ViewBag.columns = ListController.columnChoices;
+
+            return View("Index");
+        }
     }
 }
